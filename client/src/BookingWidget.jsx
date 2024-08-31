b710f2586689a80ad77c0fc6a6a7c1744c6a8113
Bao Huynh <baohuynh888@gmail.com>
2024-08-30 17:16:19 -0700

    Added functionality to booking a place and fizing styles of time and dates. Adjusted files to look cleaner and less compact

--- client/src/BookingWidget.jsx
+++ client/src/BookingWidget.jsx
@@ -1,6 +1,42 @@
+import {useState, useContext} from "react";
+import {differenceInCalendarDays} from "date-fns";
+import axios from "axios";
+import {Navigate} from "react-router-dom";
 
+export default function BookingWidget({place}) {
+    const [checkIn, setCheckIn] = useState('');
+    const [checkOut, setCheckOut] = useState('');
+    const [numberOfGuests, setNumberOfGuests] = useState(1);
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [redirect, setRedirect] = useState('');
+    const {user} = useContext(UserContext);
 
-export default BookingWidget({place}) {
+    useEffect(() => {
+        if (user) {
+            setName(user.name);
+        }
+    }, [user]);
+
+    let numberOfNights = 0;
+    if (checkIn && checkOut) {
+        numberOfNights = differenceInCalendarDays(new Date(checkOut), new Date(checkIn));
+    }
+
+    async function bookThisPlace() {
+        const response = await axios.post('/bookings', {
+            checkIn, checkOut, numberOfGuests, name, phone,
+            place: place._id, 
+            price: numberOfNights * place.price,
+        });
+        const bookingId = response.data._id;
+        setRedirect('/account/bookings/${bookingId}');
+    }
+
+    if(redirect) {
+        return <Navigate to={redirect} />
+    }
+
     return (
         <div className="bg-white shadow p-4 rounded-2xl">
             <div className="text-2xl text-center"> {/* style for price text */}
@@ -10,21 +46,42 @@ export default BookingWidget({place}) {
                 <div className="flex">
                     <div className="px-4 py-3"> {/* style for check in date element; calendar format */}
                         <label>Check-in:</label>
-                        <input type="date" />
+                        <input type="date" 
+                               value={checkIn} 
+                               onChange={ev => setCheckIn(ev.target.value)} />
                     </div>
                     <div className="px-4 py-3 border-l"> {/* style for check out element */}
                         <label>Check-out:</label>
-                        <input type="date" />
+                        <input type="date" 
+                               value={checkOut} 
+                               onChange={ev => setCheckOut(ev.target.value)} />
                     </div>
                 </div>
-                <div>
                 <div className="px-4 py-3 border-t"> {/* style for check out element */}
-                        <label>Number of guests:</label>
-                        <input type="number" value={1}/>
-                    </div>
+                    <label>Number of guests:</label>
+                    <input type="number" 
+                           value={numberOfGuests} 
+                           onChange={ev => setNumberOfGuests(ev.target.value)} />
                 </div>
+                {numberOfNights > 0 && (
+                    <div className="px-4 py-3 border-t"> {/* style for check out element */}
+                        <label>Your full name:</label>
+                        <input type="text" 
+                               value={name} 
+                               onChange={ev => setName(ev.target.value)} />
+                        <label>Phone number:</label>
+                        <input type="tel" 
+                               value={phone} 
+                               onChange={ev => setPhone(ev.target.value)} />
+                    </div>
+                )}
             </div>
-            <button className="primary mt-4">Book this place</button> {/* style for button to book place */}
+            <button onClick={bookThisPlace} className="primary mt-4">
+                Book this place
+                {numberOfNights > 0 && (
+                    <span> ${numberOfNights * place.price}</span>  //calculations of the price by multiplying # of nights with price
+                )}
+            </button> {/* style for button to book place */}
         </div>
     );
 }
